a66c6a5e738f13baf8a7df8a53548383b7b320c1
Saikat Sardar <saikatsardar5@gmail.com>
2022-06-06 12:31:47 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Welcome from "../components/Welcome";
 const index = () => {
 	return (
 		<>
-			<div className='text-center text-2xl'>
+			<div className='text-center text-4xl mt-4'>
 				<Welcome />
 			</div>
 		</>
